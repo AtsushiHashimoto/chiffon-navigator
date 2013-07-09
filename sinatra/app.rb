6e29196bfae5b576d725a6db97f334f990da49e4
Atsushi Hashimoto <a_hasimoto@mm.media.kyoto-u.ac.jp>
2013-07-09 16:24:49 +0900

    app.rb modification (not reflected in the previous commit...)

--- sinatra/app.rb
+++ sinatra/app.rb
@@ -1,9 +1,15 @@
 require 'rubygems'
 require 'sinatra'
-
+require 'lib/default.rb'
 
 get '/' do
 	'Hello world!'
+end
+
+navigators = {}
+
+configure do
+	navigators['default'] = DefaultNavigator.new()
 end
 
 options '/navi/:algorithm' do |alg|
@@ -14,8 +20,15 @@ post '/navi/:algorithm' do |alg|
 	request.body.rewind
 	data = request.body.read
 
-	output = "Algorithm: #{alg}<br>"
-	output += "JSON: #{data}"
-	return output
+	prescription;
+	if !navigator.include?(alg) then
+		# Error!!
+	elsif
+		prescription = navigator[alg].counsel(data)
+	end
+
+#	validate(prescription)
+
+	return prescription
 end
 
