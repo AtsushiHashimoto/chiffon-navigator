18365be1c648b7cc6f1fae3cd210f67649a1c7fa
Atsushi Hashimoto <a_hasimoto@mm.media.kyoto-u.ac.jp>
2013-07-19 17:01:40 +0900

    Support CORS

--- sinatra/app.rb
+++ sinatra/app.rb
@@ -11,14 +11,25 @@ end
 navigators = {}
 
 configure do
+	# add navigator algorithm class here
 	navigators['default'] = DefaultNavigator.new()
 end
 
+
 options '/navi/:algorithm' do |alg|
-	cross_origin
+	# headers against Cross Domain Request (CORS)
+	headers "Access-Control-Allow-Origin" => "*"
+	headers "Access-Control-Allow-Credentials" => "true"
+
+	headers "Access-Control-Allow-AMAethods" => "POST, OPTIONS"
+	headers "Access-Control-Max-Age" => "7200"
+	headers "Access-Control-Allow-Headers" => "x-requested-with, x-requested-by"
 end
 
 post '/navi/:algorithm' do |alg|
+	headers "Access-Control-Allow-Origin" => "*"
+	headers "Access-Control-Allow-Credentials" => "true"
+
 	request.body.rewind
 	json_data = JSON.parse(request.body.read) 
 	prescription = {}
