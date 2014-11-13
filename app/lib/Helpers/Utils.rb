8c9bbab49fcc068f73a0d592421be69c8bdcba08
Atsushi Hashimoto <a_hasimoto@mm.media.kyoto-u.ac.jp>
2014-11-13 22:55:37 +0900

    時刻のパース関数を追加

--- app/lib/Helpers/Utils.rb
+++ app/lib/Helpers/Utils.rb
@@ -18,3 +18,12 @@ class Object
         end
     end
 end
+
+class Time
+		def self.parse_my_timestamp(timestamp_str)
+			#			2014.11.13_12.43.45.471616
+			buf = /(\d{4})\.(\d{2})\.(\d{2})_(\d{2})\.(\d{2})\.(\d{2})\.(\d+)/.match(timestamp_str)
+			return nil if nil == buf[0]
+			Time.utc($1.to_i,$2.to_i,$3.to_i,$4.to_i,$5.to_i,"#{$6}.#{$7}".to_r)
+		end
+end
